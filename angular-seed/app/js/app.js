69954e2ec0dea979d168832eb51e031c6f3fcb67
oktober <farmerwebdesign@gmail.com>
2013-11-23 09:52:44 -0700

    Added random images to quiz
    Put all the sightword info into one json file and added the code to
    randomize the correct & incorrect images.

--- angular-seed/app/js/app.js
+++ angular-seed/app/js/app.js
@@ -13,6 +13,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'WordsCtrl'})
   .when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'})
   .when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'})
-  .when('/quiz/:quizId', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'})
+  .when('/quiz/:quizId', {templateUrl: 'partials/quiz.html', controller: 'QuizCtrl'})
   .otherwise({redirectTo: '/'});
 }]);
--- angular-seed/app/js/controllers.js
+++ angular-seed/app/js/controllers.js
@@ -13,8 +13,30 @@ angular.module('myApp.controllers', []).
 
   }])
   .controller('WordsCtrl', ['$scope', '$http',
-        function ($scope, $http) {
-            $http.get('data/words.json').success(function(data) {
+      function ($scope, $http) {
+            $http.get('data/words.json?' + new Date().getTime()).success(function(data) {
                 $scope.words = data;
             })
-  }]);
+  }])
+  .controller('QuizCtrl', ['$scope', '$http', '$routeParams',
+      function ($scope, $http, $routeParams) {
+          $http.get('data/words.json?' + new Date().getTime()).success(function(data) {
+              var word = data[$routeParams.quizId];
+
+              //this function will randomize which image displays first
+              var randomFunction = function(){
+                  var theNumber = Math.round(Math.random());
+                  if(theNumber === 0){
+                      $scope.img1 = word.correct_img;
+                      $scope.img2 = word.incorrect_img;
+                  }else{
+                      $scope.img1 = word.incorrect_img;
+                      $scope.img2 = word.correct_img;
+                  }
+              };
+              randomFunction();
+
+              $scope.words = word;
+          });
+  }])
+  ;
